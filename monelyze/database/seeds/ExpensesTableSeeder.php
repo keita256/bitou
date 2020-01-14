dab1fb69e09abbc81668d2745052b8a632aaa4aa
keita <39722325+keipo@users.noreply.github.com>
2020-01-14 11:29:10 +0900

    seederファイル追加
    費目テーブルのデータ

---
+++ monelyze/database/seeds/ExpensesTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ExpensesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // テーブルのクリア
+        //DB::table('expenses')->truncate();
+
+        // 費目データ
+        $expenses = [
+            [
+                'name' => '食費',
+                'description' => '毎日の食事で必要な食料や飲料、調味料など',
+            ],
+            [
+                'name' => '日用品費',
+                'description' => '生活するために必要な消耗品や住居備品',
+            ],
+            [
+                'name' => '医療費',
+                'description' => '病院にかかった際の診察代をはじめ、薬代や病院への交通費',
+            ],
+            [
+                'name' => '交通費',
+                'description' => '電車やバス、タクシーなどの公共交通機関を使用したときの費用',
+            ],
+            [
+                'name' => '交際費',
+                'description' => 'お付き合いとしての飲み会代や会食代、プレゼント代、手土産代など',
+            ],
+            [
+                'name' => '娯楽費',
+                'description' => '余暇時間・自由時間にまつわる費用',
+            ],
+        ];
+
+        // 登録
+        foreach ($expenses as $expense) {
+            \App\Expense::create($expense);
+        }
+    }
+}
