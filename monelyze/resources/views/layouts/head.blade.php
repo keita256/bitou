95b14bae9b306e93acd46068e0b398996e94a7ab
ken625 <39821597+ken625@users.noreply.github.com>
2020-01-21 16:06:21 +0900

    カレンダー作成

--- monelyze/resources/views/layouts/head.blade.php
+++ monelyze/resources/views/layouts/head.blade.php
@@ -1,14 +1,23 @@
 @section('head')
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap" rel="stylesheet">
-    <link href="{{ asset('css/hover.css') }}" rel="stylesheet">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap" rel="stylesheet">
+<link href="{{ asset('css/hover.css') }}" rel="stylesheet">
 
-    <title>Monelyze | @yield('title')</title>
+<!-- jQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Bootstrap-datepicker -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/locale/ja.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/css/tempusdominus-bootstrap-4.min.css" />
+
+<title>Monelyze | @yield('title')</title>
 @endsection
--- monelyze/resources/views/spend/spend.blade.php
+++ monelyze/resources/views/spend/spend.blade.php
@@ -6,8 +6,78 @@
 @include('layouts.header')
 
 @section('content')
-    <h1>家計簿入力画面</h1>
-    <div>
-        
+<h1>家計簿入力画面</h1>
+
+<div class="spend">
+    <div class="row">
+        <div class="col-3">費目名</div>
+        <div class="col-3">内容</div>
+        <div class="col-3">金額</div>
     </div>
-@endsection
+    <div class="row-item">
+        <div class="col-3">
+            <input type="text">
+        </div>
+        <div class="col-3">
+            <input type="text">
+        </div>
+        <div class="col-3">
+            <input type="text">
+        </div>
+    </div>
+    <div class="row-item">
+        <div class="col-3 name">
+            <input type="text">
+        </div>
+        <div class="col-3 content">
+            <input type="text">
+        </div>
+        <div class="col-3 amount">
+            <input type="text">
+        </div>
+    </div>
+    <div class="row-item">
+        <div class="col-3 name">
+            <input type="text">
+        </div>
+        <div class="col-3 content">
+            <input type="text">
+        </div>
+        <div class="col-3 amount">
+            <input type="text">
+        </div>
+    </div>
+</div>
+
+<div class="total-amount">
+    <label>合計金額</label>
+    <div class="col-9">
+        <div class="val"></div>
+        ￥
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-sm-6">
+        <div class="form-group">
+            <div class="input-group date" id="datetimepicker4" data-target-input="nearest">
+                <input type="text" class="form-control datetimepicker-input" data-target="#datetimepicker4" />
+                <div class="input-group-append" data-target="#datetimepicker4" data-toggle="datetimepicker">
+                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        $(function() {
+            $('#datetimepicker4').datetimepicker({
+                format: 'L'
+            });
+        });
+    </script>
+</div>
+
+<button type="submit">入力</button>
+
+@endsection;
