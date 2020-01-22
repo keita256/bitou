78fa79b92ca8892c848b9960d3bcff930a01336a
keita <39722325+keipo@users.noreply.github.com>
2020-01-22 13:26:37 +0900

    Update MonelyzeDB.php

--- monelyze/app/Services/MonelyzeDB.php
+++ monelyze/app/Services/MonelyzeDB.php
@@ -84,9 +84,79 @@ class MonelyzeDB
         return $spend->delete();
     }
 
-    public function getStatistics()
+    // 指定された年の月間消費額を取得(固定費を含む)
+    public function getMonthlyConsumptionAndPayment($user_id, $year)
     {
-        DB::select('select * from users where active = ?', [1]);
+        $start = $year . '0101';
+        $end = $year . '1231';
+
+        $monthly_total = DB::select(
+            'select st.month, (ifnull(st.total, 0) + ifnull(pt.total, 0)) as total from (select date_format(date, "%m") as month, sum(amount) as total from spends where user_id = ? and date between ? and ? group by date_format(date, "%m") order by month) st left outer join (select month, sum(amount) as total from payments where user_id = ? and year = ? group by month order by month) pt on st.month = pt.month',
+            [
+                $user_id,
+                $start,
+                $end,
+                $user_id,
+                $year
+            ]
+        );
+        
+        return $monthly_total;
+    }
+
+    // 指定された年の月ごとの消費額合計を取得(固定費を含まない)
+    public function getConsumption($user_id, $year)
+    {
+        $start = $year . '0101';
+        $end = $year . '1231';
+
+        $consumption = DB::select(
+            'select date_format(date, "%m") as month, sum(amount) as total from spends where user_id = :user_id and date between :start and :end group by date_format(date, "%m") order by month',
+            [
+                'user_id' => $user_id,
+                'start' => $start,
+                'end' => $end
+            ]
+        );
+
+        return $consumption;
+    }
+
+    // 指定された年月の消費額と目標支出の取得
+    public function getConsumptionAndTargetSpending($user_id, $year)
+    {
+        $start = $year . '0101';
+        $end = $year . '1231';
+
+        $consumptionAndTargetSpending = DB::select(
+            'select st.month, (ifnull(st.total, 0) + ifnull(pt.total, 0)) as total, ts.target_spending from (select date_format(date, "%m") as month, sum(amount) as total from spends where user_id = ? and date between ? and ? group by date_format(date, "%m") order by month) st left outer join (select month, sum(amount) as total from payments where user_id = ? and year = ? group by month order by month) pt on st.month = pt.month, (select month, target_spending from monthly_inputs where user_id = ? and year = ?) ts where st.month = ts.month;',
+            [
+                $user_id,
+                $start,
+                $end,
+                $user_id,
+                $year,
+                $user_id,
+                $year
+            ]
+        );
+
+        return $consumptionAndTargetSpending;
+    }
+
+    // 指定された月の費目ごとの消費額を取得
+    public function getExpenseConsumption($user_id, $year, $month)
+    {
+        $expenseConsumption = DB::select(
+            'select expenses.name, ifnull(sum(spends.amount), 0) as total from (select date, expense_id, amount from spends where user_id = :user_id and date_format(date, "%Y") = :year and date_format(date, "%m") = :month) spends right outer join expenses on spends.expense_id = expenses.expense_id group by name',
+            [
+                'user_id' => $user_id,
+                'year' => $year,
+                'month' => $month
+            ]
+        );
+
+        return $expenseConsumption;
     }
 }
 ?>
