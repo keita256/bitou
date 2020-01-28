23943c75f14a7206bee80bf6b95f37f7c09b4d43
ken625 <39821597+ken625@users.noreply.github.com>
2020-01-28 16:11:21 +0900

    spendのやーつ

--- monelyze/app/Http/Controllers/ValiController.php
+++ monelyze/app/Http/Controllers/ValiController.php
@@ -5,19 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Validator;
+use Illuminate\Support\Facades\MonelyzeDB;
 
 class ValiController extends Controller
 {
     public function receiveSpend(Request $request)
     {
-        \Log::debug($request);
+        \Log::debug($request->all());
 
         
         // バリデーションルール
         $validator = Validator::make($request->all(), [
-            'spend_id' => 'required|integer',
-            'spend_content' => 'nullable|string',
-            'spend_amount' => 'required|integer',
+
+            'spends.expense_id.*' => 'required|integer',
+            'spends.content.*' => 'nullable|string',
+            'spends.amount.*' => 'required|integer',
             'spend_date' => 'required|date',
         ]);
 
@@ -27,6 +29,9 @@ class ValiController extends Controller
                 ->withErrors($validator)
                 ->with('message', '入力に誤りがあります');
         }
+
+        
+
         
         return redirect('/spend')
             ->with('message', '入力しました');
