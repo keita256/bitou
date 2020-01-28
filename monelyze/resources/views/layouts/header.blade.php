66bb1b1dad855589c61d4354dfd90eca8a5b2b52
keita <39722325+keipo@users.noreply.github.com>
2020-01-28 14:29:39 +0900

    Update header.blade.php

--- monelyze/resources/views/layouts/header.blade.php
+++ monelyze/resources/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
         <a class="nav-link button hvr-underline-reveal" href="/spend">入力</a>
         <a class="nav-link button hvr-underline-reveal" href="/users/edit">ユーザー設定</a>
         <a class="nav-link disabled button hvr-underline-reveal" href="#" tabindex="-1" aria-disabled="true">無効</a>
-        <p class="">{{ $username }}さん</p>
+        <p class="">{{ $user_name }}さん</p>
     </nav>
 </header>
 @endsection
