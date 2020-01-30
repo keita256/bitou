8ae15acab7590457e85da804211ce21609b670dd
keita <39722325+keipo@users.noreply.github.com>
2020-01-30 09:29:58 +0900

    Update app.php

--- monelyze/config/app.php
+++ monelyze/config/app.php
@@ -225,6 +225,7 @@ return [
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
+        'StatisticsLogic' => App\Services\StatisticsLogic::class,
         'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
