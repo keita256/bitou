77b19bc51dc221563c5f7555bcac0a0616783929
keita <39722325+keita256@users.noreply.github.com>
2020-01-31 19:51:37 +0900

    Update MonthlyInputController.php

--- monelyze/app/Http/Controllers/MonthlyInputController.php
+++ monelyze/app/Http/Controllers/MonthlyInputController.php
@@ -15,38 +15,11 @@ class MonthlyInputController extends Controller
         $this->middleware('auth');
     }
 
-    public function show($year, $month)
+    public function index($year, $month)
     {
-        $user_id = Auth::id();
-
-        // 月初入力データを取得
-        $monthly_input_data = MonelyzeDB::getMonthlyInput($user_id, $year, $month);
-        $monthly_input_data = array_shit($monthly_input_data);
-
         return view('/monthly_input/index', compact(
             'year',
             'month',
-            'monthly_input_data'
-        ));
-    }
-
-    public function create(Request $request, $year, $month)
-    {
-        $user_id = Auth::id();
-        $take_amount = $request->take_amount;         // 手取り収入
-        $target_spending = $request->target_spending; // 目標支出
-
-        // データベースに登録
-        $monthly_input_data = MonelyzeDB::insertPayment($user_id, $year, $month, $take_amount, $target_spending);
-
-        // 月初入力データを取得
-        $monthly_input_data = MonelyzeDB::getMonthlyInput($user_id, $year, $month);
-        $monthly_input_data = array_shit($monthly_input_data);
-        
-        return view('/monthly_input/index',compact(
-            'year',
-            'month',
-            'monthly_input_data'
         ));
     }
 }
