f78b8cb685342bdb44c9994d46e176394277a53c
Shigee <39821453+Shigee@users.noreply.github.com>
2020-02-01 01:34:29 +0900

    月初入力をナビゲーションから排除
    デ・ス・ジ・ゴ・ク ( ；o；)…本当の爆走も知らない ( ；o；)…わずかな希望を停留させる ( ；o；)…人々を乗せて( ；o；)…共に戦いながら( ；o；)…………プェ〜ｗｗｗｗｗｗプェプェ〜ｗｗｗｗｗｗプェプェプェ〜ｗｗｗｗｗｗ

--- monelyze/resources/views/layouts/header.blade.php
+++ monelyze/resources/views/layouts/header.blade.php
@@ -9,7 +9,6 @@
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0 mr-auto">
                 <li><a class="nav-item nav-link button" href="/spend">入力</a></li>
                 <li><a class="nav-item nav-link button" href="/users/edit">ユーザー設定</a></li>
-                <li><a class="nav-item nav-link button" href="/monthlyInput">月初入力</a></li>
                 <li><a class="nav-item nav-link button" href="/payment">固定費入力</a></li>
                 <li><a class="nav-item nav-link button" href="/statistics">年度別統計</a></li>
             </ul>
