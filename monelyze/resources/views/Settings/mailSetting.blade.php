991d3075678c22e0e9c76181a522971c51bdc7be
Shigee <39821453+Shigee@users.noreply.github.com>
2020-02-01 00:18:10 +0900

    せっていがｍｒん

---
+++ monelyze/resources/views/Settings/mailSetting.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.common')
+
+@section('title', 'ユーザー設定')
+@include('layouts.head')
+
+@include('layouts.header')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="offset-md-3 col-md-6 offset-sm-1 col-sm-10 monthlyInput-div">
+            <h3>メールアドレスの変更</h3>
+
+            @if (Session::has('message'))
+            <p>{{ session('message') }}</p>
+            @endif
+
+            <div class="row">
+                <div class="offset-md-2 col-md-8 offset-sm-1 col-sm-10 inner-div rounded">
+                    <form action="/users/mailSetting" method="post" accept-charset="UTF-8">
+
+                        <div class="form-group">
+                            <label for="">新しいメールアドレス</label>
+                            <input class="form-control" required type="email" value="">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary float-right">保存</button>
+                    </form>
+                </div>
+            </div>
+        </div><!-- col -->
+    </div><!-- row -->
+</div><!-- container -->
+@endsection
---
+++ monelyze/resources/views/Settings/nameSetting.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.common')
+
+@section('title', 'ユーザー設定')
+@include('layouts.head')
+
+@include('layouts.header')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="offset-md-3 col-md-6 offset-sm-1 col-sm-10 monthlyInput-div">
+            <h3>ユーザー名の変更</h3>
+
+            @if (Session::has('message'))
+            <p>{{ session('message') }}</p>
+            @endif
+
+            <div class="row">
+                <div class="offset-md-2 col-md-8 offset-sm-1 col-sm-10 inner-div rounded">
+                    <form action="/users/nameSetting" method="post" accept-charset="UTF-8">
+                        <div class="form-group">
+                            <label for="">新しいユーザー名</label>
+                            <input class="form-control" required type="text" value="">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary float-right">保存</button>
+                    </form>
+                </div>
+            </div>
+        </div><!-- col -->
+    </div><!-- row -->
+</div><!-- container -->
+@endsection
