72977db962318cd320177d6412b9fe58104f50cc
Shigee <39821453+Shigee@users.noreply.github.com>
2020-01-31 11:05:51 +0900

    ナビゲーション修正
    下部のマージンとハンバーガーメニュー化したときのメニューを中央揃え
    あとは色合いとかマウスホバー時のアクションとかぐらいか

--- monelyze/resources/views/layouts/header.blade.php
+++ monelyze/resources/views/layouts/header.blade.php
@@ -1,17 +1,17 @@
 @section('header')
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <nav class="navbar navbar-expand-lg navbar-light" style="margin-bottom: 1em">
         <a class="navbar-brand" href="/home">Monelyze</a>
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="レスポンシブ・ナビゲーションバー">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="Navber">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="active"><a class="nav-item nav-link active button hvr-underline-reveal" href="/home">ホーム</a></li>
-                <li><a class="nav-item nav-link button hvr-underline-reveal" href="/spend">入力</a></li>
-                <li><a class="nav-item nav-link button hvr-underline-reveal" href="/users/edit">ユーザー設定</a></li>
-                <li><a class="nav-item nav-link button hvr-underline-reveal" href="/monthlyInput">月初入力</a></li>
-                <li><a class="nav-item nav-link button hvr-underline-reveal" href="/payment">固定費入力</a></li>
+                <li class="active"><a class="nav-item nav-link active button" href="/home">ホーム</a></li>
+                <li><a class="nav-item nav-link button" href="/spend">入力</a></li>
+                <li><a class="nav-item nav-link button" href="/users/edit">ユーザー設定</a></li>
+                <li><a class="nav-item nav-link button" href="/monthlyInput">月初入力</a></li>
+                <li><a class="nav-item nav-link button" href="/payment">固定費入力</a></li>
                 @if(Auth::check())
                 <li><p class="nav-item">{{ Auth::user()->name }}さん</p></li>
                 @endif
