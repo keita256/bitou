face31e3efe3da5c6ce52b47c4614e7685ee7124
Shigee <39821453+Shigee@users.noreply.github.com>
2020-01-31 13:41:02 +0900

    レスポンシブ化
    ユーザ設定ページ

--- monelyze/resources/views/user.blade.php
+++ monelyze/resources/views/user.blade.php
@@ -8,7 +8,7 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="offset-3 col-6 monthlyInput-div">
+        <div class="offset-md-3 col-md-6 offset-sm-1 col-sm-10 monthlyInput-div">
             <h3>ユーザー設定</h3>
 
             @if (Session::has('message'))
@@ -16,7 +16,7 @@
             @endif
 
             <div class="row">
-                <div class="offset-2 col-8 inner-div rounded">
+                <div class="offset-md-2 col-md-8 offset-sm-1 col-sm-10 inner-div rounded">
                     <form action="/user/edit" method="post" accept-charset="UTF-8">
                         <div class="form-group">
                             <label for="">ユーザー名</label>
