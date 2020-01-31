7664c8e11e8ddf4bb2849d39ca1514082574bb59
Shigee <39821453+Shigee@users.noreply.github.com>
2020-01-31 18:17:49 +0900

    パスワードを忘れた
    メール送信前まで

--- monelyze/resources/views/auth/passwords/email.blade.php
+++ monelyze/resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,45 @@
-@extends('layouts.app')
+@extends('layouts.common')
 
+@section('title', 'パスワードをリセット')
+@include('layouts.head')
 <!-- Main Content -->
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+        <div class="col-10 offset-1 col-lg-4 offset-lg-4 mt-5">
+            <div class="mb-3">
+                <h1 style="text-align: center;">パスワードをリセット</h1>
             </div>
+            @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+            @endif
+
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+                {{ csrf_field() }}
+
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="control-label">メールアドレス</label>
+
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                    @if ($errors->has('email'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <a class="btn btn-link" href="{{ url('/login') }}">
+                        ログイン画面に戻る
+                    </a>
+                    <button type="submit" class="btn btn-primary float-right">
+                        パスワードをリセット
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
