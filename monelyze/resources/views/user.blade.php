f84c0bd9730858df0a3a7690c3ee6523927ed731
Shigee <39821453+Shigee@users.noreply.github.com>
2020-02-01 01:09:31 +0900

    うせｒせっちんｇ

--- monelyze/resources/views/user.blade.php
+++ monelyze/resources/views/user.blade.php
@@ -17,20 +17,14 @@
 
             <div class="row">
                 <div class="offset-md-2 col-md-8 offset-sm-1 col-sm-10 inner-div rounded">
-                        <div class="">
-                            <label class="float-left">現在のユーザー名：</label>
-                            <label>{{$username}}</label>
-                            <a class="btn btn-link" href="/users/nameSetting">
-                                変更
-                            </a>
+                        <div class="mb-3">
+                            <label class="float-left mt-1 mb-4">現在のユーザー名：{{$username}}</label>
+                            <a href="/users/nameSetting"><button class="btn btn-primary float-right mb-3">変更</button></a>
                         </div>
 
-                        <div class="clearfix">
-                            <label class="float-left">現在のメールアドレス：</label>
-                            <label>{{$mail}}</label>
-                            <a class="btn btn-link" href="/users/mailSetting">
-                                変更
-                            </a>
+                        <div class="mb-3">
+                            <label class="float-left mt-2 mb-1">現在のメールアドレス：{{$mail}}</label>
+                            <a href="/users/mailSetting"><button class="btn btn-primary float-right mb-1">変更</button></a>
                         </div>
                 </div>
             </div>
