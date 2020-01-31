291bbd59a3863e8e04cfdcb8d9436daff0f31c79
keita <39722325+keita256@users.noreply.github.com>
2020-01-31 11:49:30 +0900

    Update MonthlyInputController.php

--- monelyze/app/Http/Controllers/MonthlyInputController.php
+++ monelyze/app/Http/Controllers/MonthlyInputController.php
@@ -15,30 +15,15 @@ class MonthlyInputController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
-    {
-        $year = date('Y', time());
-        $month = date('m', time());
-        $username = Auth::user()->name;
-
-        return view('/monthly_input/index', compact(
-            'username',
-            'year',
-            'month'
-        ));
-    }
-
     public function show($year, $month)
     {
         $user_id = Auth::id();
-        $username = Auth::user()->name;
 
         // 月初入力データを取得
         $monthly_input_data = MonelyzeDB::getMonthlyInput($user_id, $year, $month);
         $monthly_input_data = array_shit($monthly_input_data);
 
         return view('/monthly_input/index', compact(
-            'username',
             'year',
             'month',
             'monthly_input_data'
@@ -48,15 +33,20 @@ class MonthlyInputController extends Controller
     public function create(Request $request, $year, $month)
     {
         $user_id = Auth::id();
-        $username = Auth::user()->name;
-        $take_amount = $request->take_amount;
-        $target_spending = $request->target_spending;
+        $take_amount = $request->take_amount;         // 手取り収入
+        $target_spending = $request->target_spending; // 目標支出
 
-        // 登録
-        MonelyzeDB::insertPayment();
+        // データベースに登録
+        $monthly_input_data = MonelyzeDB::insertPayment($user_id, $year, $month, $take_amount, $target_spending);
+
+        // 月初入力データを取得
+        $monthly_input_data = MonelyzeDB::getMonthlyInput($user_id, $year, $month);
+        $monthly_input_data = array_shit($monthly_input_data);
         
         return view('/monthly_input/index',compact(
-            ''
+            'year',
+            'month',
+            'monthly_input_data'
         ));
     }
 }
