e57b0cc388f8397e33215da6b9765a15372faf91
keita <39722325+keita256@users.noreply.github.com>
2020-01-31 10:53:28 +0900

    Update login.blade.php

--- monelyze/resources/views/auth/login.blade.php
+++ monelyze/resources/views/auth/login.blade.php
@@ -1,67 +1,71 @@
-@extends('layouts.app')
+@extends('layouts.common')
+
+@section('title', 'ログイン')
+@include('layouts.head')
 
 @section('content')
-<div class="container">
+<div class="container-fluid ">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+        <div class="col-lg-6 offset-lg-3">
+            <h1 class="text-center">Monelyze</h1>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+            <form action="{{ url('/login') }}" method="POST">
+                {{ csrf_field() }}
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="col-6 offset-3 control-label">メールアドレス</label>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                    <div class="col-6 offset-3">
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
                 </div>
-            </div>
+
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="col-6 offset-3 control-label">パスワード</label>
+
+                    <div class="col-6 offset-3">
+                        <input id="password" type="password" class="form-control" name="password" required>
+
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+
+                        <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                            パスワードを忘れた場合
+                        </a>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-6 offset-3">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> 記憶する
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-6 offset-3">
+                        <a class="btn btn-link" href="{{ url('/register') }}">
+                            アカウントを作成
+                        </a>
+
+                        <button type="submit" class="btn btn-primary float-right">
+                            ログイン
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
