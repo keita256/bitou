4af4859f785a964f8cb520ccae1e7e614e6b5537
Shigee <39821453+Shigee@users.noreply.github.com>
2020-02-01 18:27:04 +0900

    logout add ❤

---
+++ header.blade.php
@@ -0,0 +1,24 @@
+@section('header')
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light" style="margin-bottom: 1em">
+        <a class="navbar-brand" href="/home">Monelyze</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="レスポンシブ・ナビゲーションバー">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="Navber">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 mr-auto">
+                <li><a class="nav-item nav-link button" href="/spend">入力</a></li>
+                <li><a class="nav-item nav-link button" href="/users/edit">ユーザー設定</a></li>
+                <li><a class="nav-item nav-link button" href="/payment">固定費入力</a></li>
+                <li><a class="nav-item nav-link button" href="/statistics">年度別統計</a></li>
+            </ul>
+            @if(Auth::check())
+            <ul class="navbar-nav">{{ Auth::user()->name }}さん</ul>
+            @endif
+            @if(Auth::check())
+            <ul class="navbar-nav"><a href="/logout">ログアウト</a></ul>
+            @endif
+        </div><!-- /.navbar-collapse -->
+    </nav>
+</header>
+@endsection
