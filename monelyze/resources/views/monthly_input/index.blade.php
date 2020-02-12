ec09b4283e6f6e877fd29df222fe486fad721f74
Shigee <39821453+Shigee@users.noreply.github.com>
2020-02-12 10:44:24 +0900

    イ

--- monelyze/resources/views/monthly_input/index.blade.php
+++ monelyze/resources/views/monthly_input/index.blade.php
@@ -11,11 +11,11 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="offset-md-3 col-md-6 offset-sm-1 col-sm-10 monthlyInput-div">
+        <div class="offset-lg-2 col-lg-8 offset-md-2 col-md-8 offset-sm-1 col-sm-10 monthlyInput-div">
             <h3>月初入力</h3>
 
             <div class="row">
-                <div class="offset-md-2 col-md-8 offset-sm-1 col-sm-10 inner-div rounded">
+                <div class="offset-1 col-10 offset-md-1 col-md-10 offset-sm-1 col-sm-10 inner-div rounded">
                     <h2>{{ $year }}年{{ $month }}月</h2>
 
                     <form action="/monthlyInput/{{ $year }}/{{ $month }}/{{ $day }}" method="post">
