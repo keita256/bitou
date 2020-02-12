6de1df7f31feb480df4a322cd53107944f3daba9
Shigee <39821453+Shigee@users.noreply.github.com>
2020-02-12 10:45:15 +0900

    イク

--- monelyze/resources/views/user.blade.php
+++ monelyze/resources/views/user.blade.php
@@ -8,7 +8,7 @@
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="offset-md-3 col-md-6 offset-sm-1 col-sm-10 monthlyInput-div">
+        <div class="offset-lg-2 col-lg-8 offset-md-2 col-md-8 offset-sm-1 col-sm-10 monthlyInput-div">
             <h3>ユーザー設定</h3>
 
             @if (Session::has('message'))
@@ -16,7 +16,7 @@
             @endif
 
             <div class="row ">
-                <div class="offset-md-2 col-md-8 offset-sm-1 col-sm-10 inner-div rounded ">
+                <div class="offset-1 col-10 offset-md-1 col-md-10 offset-sm-1 col-sm-10 inner-div rounded ">
                     <div class="mb-3 clearfix">
                         <label class="float-left mt-1 mb-4">現在のユーザー名：{{$username}}</label>
                         <a href="/users/nameSetting"><button class="btn btn-primary float-right  mb-3 ">変更</button></a>
