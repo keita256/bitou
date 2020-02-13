ab5f9491084ca491a33bc4a49ac313f6f9beaa39
keita <39722325+keipo@users.noreply.github.com>
2020-02-13 11:03:54 +0900

    Update user.blade.php

--- monelyze/resources/views/user.blade.php
+++ monelyze/resources/views/user.blade.php
@@ -16,7 +16,7 @@
             @endif
 
             <div class="row ">
-                <div class="offset-1 col-10 offset-md-1 col-md-10 offset-sm-1 col-sm-10 inner-div rounded ">
+                <div class="offset-1 col-10 offset-md-1 col-md-10 offset-sm-1 col-sm-10 inner-div rounded">
                     <div class="mb-3 clearfix">
                         <label class="float-left mt-1 mb-4">現在のユーザー名：{{$username}}</label>
                         <a href="/users/nameSetting"><button class="btn btn-primary float-right  mb-3 ">変更</button></a>
