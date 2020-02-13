e4a83262ba860c7f17c4ad154cdec40e4705e401
Taka <57925441+Ma-p1e@users.noreply.github.com>
2020-02-13 14:53:28 +0900

    使わないもの削除

--- monelyze/resources/views/layouts/head.blade.php
+++ monelyze/resources/views/layouts/head.blade.php
@@ -14,8 +14,6 @@
 
 <!-- jQuery -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="{{ asset('js/anime.min.js') }}"></script>
-<script src="{{ asset('js/animation.js') }}"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/locale/ja.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
